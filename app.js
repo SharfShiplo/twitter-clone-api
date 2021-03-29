a7cfc7e9931c5cb4a42a9072c119b380ef037f33
SharfShiplo <sharf.ahammed@gmail.com>
2021-03-29 21:57:03 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Twitter = require('./api/helpers/twitter');
+const twitter = new Twitter();
+require('dotenv').config()
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next()
+})
+app.get('/tweets', (req, res) => {
+    const query = req.query.q;
+    const count = req.query.count;
+    const maxId = req.query.max_id;
+    twitter.get(query, count, maxId).then((response) => {
+        res.status(200).send(response.data)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.listen(port, () => console.log(`Twitter APP listening on port ${port}`))
